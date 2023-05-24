ffc149fd0a30f6f1f78e1421619fa047c554550f
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-05-23 21:54:15 -0300

    Animate Scroll

--- script.js
+++ script.js
@@ -66,3 +66,27 @@ function initSmoothScroll() {
 }
 
 initSmoothScroll();
+
+function initAnimateScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    // metodo de ja deixar a classe sections[0].classList.add("ativo");
+
+    function animateScroll() {
+      sections.forEach((section) => {
+        const topSection = section.getBoundingClientRect().top;
+        const isSectionVisible = topSection - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    // para quando o usuário abrir o site e não ter tela branca
+    animateScroll();
+
+    window.addEventListener("scroll", animateScroll);
+  }
+}
+initAnimateScroll();
