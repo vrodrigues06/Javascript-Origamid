a28d87389b1ff9f3fdac8d8627dc3fd13d70b08b
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-06-16 17:45:13 -0300

    Modal add

---
+++ js/modules/modal.js
@@ -0,0 +1,20 @@
+export default function initModal() {
+  const modalContainer = document.querySelector('[data-modal="container"]');
+  const closeButton = document.querySelector('[data-modal="close"]');
+  const openButton = document.querySelector('[data-modal="open"]');
+
+  if (closeButton && openButton && modalContainer) {
+    function toggleModal(event) {
+      event.preventDefault();
+      modalContainer.classList.toggle("ativo");
+    }
+    function clickOutModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+    openButton.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    modalContainer.addEventListener("click", clickOutModal);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from "./modules/smooth-scroll.js";
 import initAnimateScroll from "./modules/animate-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/navigation-tab.js";
+import initModal from "./modules/modal.js";
 
 initTabNav();
 initAccordion();
 initSmoothScroll();
 initAnimateScroll();
+initModal();
