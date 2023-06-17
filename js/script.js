c471578dbc82eac004d89119d8047bce80961fca
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-06-17 18:23:14 -0300

    Tooltip add

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimateScroll from "./modules/animate-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/navigation-tab.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initTabNav();
 initAccordion();
 initSmoothScroll();
 initAnimateScroll();
 initModal();
+initTooltip();
