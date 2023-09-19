5673fc1942ed6873fb16fcddc40d9f3f7cac6b08
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-09-19 12:47:56 -0300

    posicao animate-scroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import SmoothScroll from "./modules/smooth-scroll.js";
-import initAnimateScroll from "./modules/animate-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/navigation-tab.js";
 import initModal from "./modules/modal.js";
@@ -9,6 +8,7 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initAnimaisFetch from "./modules/animais-fetch.js";
 import initFetchBtc from "./modules/fetch-btc.js";
+import initAnimateScroll from "./modules/animate-scroll.js";
 
 const smoothScroll = new SmoothScroll('[data-menu="suave"] a[href^="#"]');
 smoothScroll.init();
