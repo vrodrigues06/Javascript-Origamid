8b439302afc09acf4e70078db608655c00380348
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-09-22 13:26:04 -0300

    Criada Function Debounce

--- js/modules/animate-scroll.js
+++ js/modules/animate-scroll.js
@@ -1,11 +1,14 @@
+import debounce from "./debounce.js";
+
 export default class AnimateScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
+  // Pega a distância de cada item em relação ao topo do site
   getDistance() {
     this.distance = [...this.sections].map((section) => {
       const offset = section.offsetTop;
@@ -15,7 +18,9 @@ export default class AnimateScroll {
       };
     });
   }
+  //
 
+  // verifica a distância em cada objeto em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
       if (window.pageYOffset > item.offset) {
@@ -33,5 +38,10 @@ export default class AnimateScroll {
       window.addEventListener("scroll", this.checkDistance);
     }
     return this;
+  }
+
+  // Remove o event de scroll para parar animação
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
